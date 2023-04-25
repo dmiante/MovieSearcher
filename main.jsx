78689f532a8c4e7290a181742b3b5a954a88e0ba
damian <the.dmian@live.cl>
2023-04-24 20:06:05 -0400

    ADD tailwind

--- main.jsx
+++ main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './src/App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './src/App'
+import './global.css'
 
-const root = createRoot(document.getElementById('app'))
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('app'))
+  .render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
